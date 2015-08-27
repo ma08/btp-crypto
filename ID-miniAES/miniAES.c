8a7a8cd47934c86d0682c1dfc7827f38ed280e7a
mao8 <thisisma08@gmail.com>
2015-08-27 21:31:55 +0530

    Read sbox from file
    Sbox is read from file "sbox.txt" in which each line containts
    index and the substituting value

--- ID-miniAES/miniAES.c
+++ ID-miniAES/miniAES.c
@@ -1,4 +1,34 @@
 #include <stdio.h>
+#include<stdlib.h>
+
+char readNibble(char *s){
+	char output = 0;
+	int i;
+	for ( i = 0; i < 4; ++i )
+		output |= (s[i] == '1') << (3 - i);
+	return output;
+}
+
+void readSbox(const char* filename, char *sbox){
+	FILE * fp;
+	char c[8],d[8];
+	const size_t line_size = 300;
+	char* line = (char *)malloc(line_size);
+	fp = fopen( "sbox.txt" , "r");
+	if(fp == NULL)
+		return;
+	while (fgets(line, line_size, fp) != NULL)  {
+		sscanf(line,"%s %s",c,d);
+		int index = readNibble(c);
+		char out = readNibble(d);
+		sbox[index] = out;
+		/*printf("%d %d\n",index,(int)(sbox[index]));*/
+	}
+	free(line);
+	fclose(fp);
+}
+
+
 
 void getInput(char X[4])
 {
